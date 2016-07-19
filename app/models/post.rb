02af37458b5b1b71b2b718beaa9c7d3402186424
Roy Wei <roywjy@gmail.com>
2016-07-19 17:31:52 +0800

    model testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  has_many :comments
+
+  def self.no_content
+    where(content: nil)
+  end
+
+  def abstract
+    self.content[0..20]
+  end
+
 end
